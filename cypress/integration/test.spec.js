1325a9d4cc4d73bc11d8d5a9a61be678367eb6c8
Tobias Baden <tobias.baden@t-online.de>
2020-02-28 10:14:41 +0100

    Commit to test the PR integration

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -2,4 +2,8 @@ describe('My First Test', function() {
   it('Does not do much!', function() {
     expect(true).to.equal(true)
   })
+  
+  it('Tests the PR feature', function() {
+    expect(true).to.equal(true)
+  })
 })
